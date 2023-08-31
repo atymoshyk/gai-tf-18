83469e41b7e19c75c7db560ae77bb2226c7bbced
Andrii Tymoshyk <andrii.tymoshyk@gmail.com>
2023-08-31 19:04:58 +0300

    add missing files

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value = module.networking.vpc_id
+}
+
+output "public_subnets" {
+  value = module.networking.public_subnets
+}
+
+output "private_subnets" {
+  value = module.networking.private_subnets
+}
+
+output "instances" {
+  value = module.instances
+}
